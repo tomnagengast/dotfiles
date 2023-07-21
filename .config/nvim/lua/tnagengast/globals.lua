e2da43b8379d40774b973a9ae2a8016543af2125
tnagengast <tnagengast@gmail.com>
2023-07-21 13:24:16 -0700

    Ensure newline at EOF

--- .config/nvim/lua/tnagengast/globals.lua
+++ .config/nvim/lua/tnagengast/globals.lua
@@ -15,3 +15,18 @@ R = function(name)
     RELOAD(name)
     return require(name)
 end
+
+-- Ensure that there is always a newline at the end of the file
+_G.add_new_line = function()
+  local n_lines = vim.api.nvim_buf_line_count(0)
+  local last_nonblank = vim.fn.prevnonblank(n_lines)
+  if last_nonblank <= n_lines then vim.api.nvim_buf_set_lines(0, last_nonblank, n_lines, true, { '' }) end
+end
+
+vim.cmd([[
+  augroup AddNewlineOnSave
+    autocmd!
+    autocmd BufWritePre * lua _G.add_new_line()
+  augroup END
+]])
+

d1d24a527d1a7629a1246855b9bb0b7128be9a1a
tomnagengast <tnagengast@gmail.com>
2024-10-15 23:02:22 -0700

    Add aliases

--- home.nix
+++ home.nix
@@ -85,85 +85,87 @@
       plugins = ["git" "direnv"];
     };
 
-    shellAliases = {
-      # SSH
-      gogogo = "ssh -i ~/.ssh/replit -p 22 5dbe8df1-a623-4cc2-a30e-c954e2114be0@5dbe8df1-a623-4cc2-a30e-c954e2114be0-00-20lqwhdcc1630.kirk.replit.dev";
+    shellAliases = builtins.fromJSON (builtins.readFile ./zsh/aliases.nix);
 
-      # Navigation
-      ".." = "cd ..";
-      "..." = "cd ../..";
-      "...." = "cd ../../..";
-      df = "vim $HOME/dotfiles";
-      dfa = "vim $HOME/dotfiles/aliases.zsh";
-      dfz = "vim $HOME/dotfiles/zshrc.zsh";
+    # shellAliases = {
+    #   # SSH
+    #   gogogo = "ssh -i ~/.ssh/replit -p 22 5dbe8df1-a623-4cc2-a30e-c954e2114be0@5dbe8df1-a623-4cc2-a30e-c954e2114be0-00-20lqwhdcc1630.kirk.replit.dev";
 
-      # System
-      ls = "lsd";
-      cat = "bat";
-      reloaddns = "dscacheutil -flushcache && sudo killall -HUP mDNSResponder";
-      l = "lsd -1A --group-directories-first";
-      ll = "lsd -1Al --header --group-directories-first";
-      s = "subl";
-      vim = "nvim";
-      h = "history | tail -n 50";
+    #   # Navigation
+    #   ".." = "cd ..";
+    #   "..." = "cd ../..";
+    #   "...." = "cd ../../..";
+    #   df = "vim $HOME/dotfiles";
+    #   dfa = "vim $HOME/dotfiles/aliases.zsh";
+    #   dfz = "vim $HOME/dotfiles/zshrc.zsh";
 
-      # Directories
-      work = "cd $HOME/code/replicatedhq/data";
-      me = "cd $HOME/code/tomnagengast";
+    #   # System
+    #   ls = "lsd";
+    #   cat = "bat";
+    #   reloaddns = "dscacheutil -flushcache && sudo killall -HUP mDNSResponder";
+    #   l = "lsd -1A --group-directories-first";
+    #   ll = "lsd -1Al --header --group-directories-first";
+    #   s = "subl";
+    #   vim = "nvim";
+    #   h = "history | tail -n 50";
 
-      # dbt
-      dl = "uv run dbt ls -s state:modified --resource-type model";
-      dr = "uv run dbt run -s state:modified";
-      drm = "uv run dbt run -s";
-      db = "uv run dbt build -s state:modified";
-      dbm = "uv run dbt build -s";
-      dt = "uv run dbt test -m state:modified";
-      dtm = "uv run dbt test";
+    #   # Directories
+    #   work = "cd $HOME/code/replicatedhq/data";
+    #   me = "cd $HOME/code/tomnagengast";
 
-      # GitHub
-      npr = "gh pr create --web";
-      cpb = "git branch | sed -n -e 's/^\* \(.*\)/\1/p' | pbcopy";
-      lpr = "cat ~/code/replicatedhq/__templates/dbt_pr.md | pbcopy";
-      runs = "gh run watch -R $(git remote get-url origin | sed -E 's|.*github\.com[:/](.+/[^/]+)\.git|\\1|') .git)";
+    #   # dbt
+    #   dl = "uv run dbt ls -s state:modified --resource-type model";
+    #   dr = "uv run dbt run -s state:modified";
+    #   drm = "uv run dbt run -s";
+    #   db = "uv run dbt build -s state:modified";
+    #   dbm = "uv run dbt build -s";
+    #   dt = "uv run dbt test -m state:modified";
+    #   dtm = "uv run dbt test";
 
-      # Snowflake
-      snowsql = "/Applications/SnowSQL.app/Contents/MacOS/snowsql";
-      snf = "snowsql";
+    #   # GitHub
+    #   npr = "gh pr create --web";
+    #   cpb = "git branch | sed -n -e 's/^\* \(.*\)/\1/p' | pbcopy";
+    #   lpr = "cat ~/code/replicatedhq/__templates/dbt_pr.md | pbcopy";
+    #   runs = "gh run watch -R $(git remote get-url origin | sed -E 's|.*github\.com[:/](.+/[^/]+)\.git|\\1|') .git)";
 
-      # Laravel
-      art = "php artisan";
-      a = "php artisan";
-      fresh = "php artisan migrate:fresh --seed";
+    #   # Snowflake
+    #   snowsql = "/Applications/SnowSQL.app/Contents/MacOS/snowsql";
+    #   snf = "snowsql";
 
-      # Git
-      clean-local-branches = "git branch | grep -v \"main\" | xargs git branch -D";
-      amend = "git add -A && git commit --amend --no-edit";
-      ga = "git add -A && git commit -m";
-      nah = "git clean -df && git reset --hard";
-      oops = "git reset --soft HEAD~1";
-      pull = "git pull";
-      push = "git push";
-      resolve = "git add . && git commit --no-edit";
-      unstage = "git restore --staged .";
-      wip = "ga wip";
-      wipp = "wip && push";
-      aye = "git checkout";
-      gl = "glo";
-      gs = "gss";
-      gr = "git recent -n 5";
-      ggo = "g go";
+    #   # Laravel
+    #   art = "php artisan";
+    #   a = "php artisan";
+    #   fresh = "php artisan migrate:fresh --seed";
 
-      # Utilities
-      cleanpy = "find . -type d -name '__pycache__' -exec rm -r {} +";
-      cleands = "find . -type f -name '*.DS_Store' -ls -delete";
-      cleanbak = "find . -type f -name '*.bak' -ls -delete";
-      cleanup = "cleanpy && cleands && cleanbak";
-      week = "date +%V";
-      localip = "ipconfig getifaddr en0";
-      hidedesktop = "defaults write com.apple.finder CreateDesktop -bool false && killall Finder";
-      showdesktop = "defaults write com.apple.finder CreateDesktop -bool true && killall Finder";
-      path = "echo $PATH | tr ':' '\\n'";
-    };
+    #   # Git
+    #   clean-local-branches = "git branch | grep -v \"main\" | xargs git branch -D";
+    #   amend = "git add -A && git commit --amend --no-edit";
+    #   ga = "git add -A && git commit -m";
+    #   nah = "git clean -df && git reset --hard";
+    #   oops = "git reset --soft HEAD~1";
+    #   pull = "git pull";
+    #   push = "git push";
+    #   resolve = "git add . && git commit --no-edit";
+    #   unstage = "git restore --staged .";
+    #   wip = "ga wip";
+    #   wipp = "wip && push";
+    #   aye = "git checkout";
+    #   gl = "glo";
+    #   gs = "gss";
+    #   gr = "git recent -n 5";
+    #   ggo = "g go";
+
+    #   # Utilities
+    #   cleanpy = "find . -type d -name '__pycache__' -exec rm -r {} +";
+    #   cleands = "find . -type f -name '*.DS_Store' -ls -delete";
+    #   cleanbak = "find . -type f -name '*.bak' -ls -delete";
+    #   cleanup = "cleanpy && cleands && cleanbak";
+    #   week = "date +%V";
+    #   localip = "ipconfig getifaddr en0";
+    #   hidedesktop = "defaults write com.apple.finder CreateDesktop -bool false && killall Finder";
+    #   showdesktop = "defaults write com.apple.finder CreateDesktop -bool true && killall Finder";
+    #   path = "echo $PATH | tr ':' '\\n'";
+    # };
   };
 
   programs.bat = {
@@ -220,11 +222,12 @@
     };
   };
 
+  # programs.tmux = import ./tmux/tmux.nix;
   programs.tmux = {
     enable = true;
     shell = "${pkgs.zsh}/bin/zsh";
     # Import the tmux configuration from tmux/tmux.nix
-    extraConfig = builtins.readFile ./tmux/tmux.nix;
+    extraConfig = builtins.readFile ./tmux/tmux.conf;
     plugins = with pkgs.tmuxPlugins; [
       # Add any tmux plugins you want to use here
     ];
